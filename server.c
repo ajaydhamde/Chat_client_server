077d3e5378bf4146db4c14d29c4e2d285b6b373d
ajaydhamde <ajay.dhamde@gmail.com>
2022-12-03 22:46:09 +0530

    server file is added

---
+++ server.c
@@ -0,0 +1,12 @@
+#incldue <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(){
+	
+	return 0;
+}
